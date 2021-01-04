c8b6dafd04c019565feb0a5004ed0857857d67aa
enolgor <enolgor@teleco.upv.es>
2021-01-04 16:02:04 +0100

    Fix hooks

--- install.sh
+++ install.sh
@@ -6,15 +6,16 @@ sudo mkdir -p "$DIR/enabled"
 sudo mkdir -p "$DIR/available"
 sudo curl -s https://raw.githubusercontent.com/enolgor/dcompose/master/dcompose -o /usr/bin/dcompose
 sudo chmod a+x /usr/bin/dcompose
+cd $DIR
 if [ ! -f on_enable ]; then
-cat > on_enable <<- EOM
+sudo cat > on_enable <<- EOM
 echo "Enabled $1"
 EOM
-chmod +x on_enable
+sudo chmod +x on_enable
 fi
 if [ ! -f on_disable ]; then
-cat > on_enable <<- EOM
+sudo cat > on_disable <<- EOM
 echo "Disabled $1"
-chmod +x on_disable
+sudo chmod +x on_disable
 EOM
 fi
